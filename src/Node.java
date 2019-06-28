e4d5fa308965a11e33e9294a8a89b7c73b2d75c1
carol <carol@DESKTOP-BOG65QU>
2019-06-28 02:09:27 -0300

    testando se foi

---
+++ src/Node.java
@@ -0,0 +1,66 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author carol
+ */
+public class Node {
+    
+    private Node parent;    
+    private boolean isTerminal;
+    private char symbol;
+    private int index;
+    
+    public List<Node> children;
+    
+    public Node(char s, Node p, int i)
+    {
+        this.parent = p;
+        this.symbol = s;
+        this.index = i;
+        
+        this.children = new ArrayList<>();
+    }
+    
+    public Node getParent()
+    {
+        return this.parent;
+    }
+    
+    public void setParent(Node p)
+    {
+        this.parent = p;
+    }
+    
+    public boolean isTerminal()
+    {
+        return this.isTerminal;
+    }
+    
+    public void setIfIsTerminal(boolean isT)
+    {
+        this.isTerminal = isT;
+    } 
+    
+    public char getSymbol()
+    {
+        return this.symbol;
+    }
+    
+    public void setSymbol(char c)
+    {
+        this.symbol = c;
+    }
+    
+    public void AddChildNode(Node n)
+    {
+        children.add(n);
+    }
+}
